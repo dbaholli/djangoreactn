7b81b22d0391420bf2ca67af5fd6ba72228108f2
diell baholli <diell@asist.space>
2021-12-20 02:56:19 +0100

    rest api

---
+++ APIProject/api/urls.py
@@ -0,0 +1,21 @@
+from django.db import router
+from django.urls import path, include
+from .views import ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+
+#article_list, article_details
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
+
+
+urlpatterns = [
+    path('', include(router.urls))
+    # path('articles/', ArticleList.as_view()),
+    # path('articles/<int:id>/', ArticleDetails.as_view())
+
+    #path('articles/', article_list),
+    #path('articles/<int:pk>/', article_details),
+
+]
